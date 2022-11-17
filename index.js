e8b57c100b8f2fa3130a16a6c844d969d2fb0d43
nbogie <neillbogie@googlemail.com>
2022-11-17 00:29:57 +0000

    load js from file before passing to jsdoc parser

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
+const fs = require('fs').promises;
 const jsdocParse = require("@jsdoc/parse")
-const ast = jsdocParse.AstBuilder.build("", "./fileUnderStudy.js")
-// console.dir(ast.program, { depth: 100 })
-console.dir(ast, { depth: 100 })
+
+
+
+async function getFileContents(fileName) {
+    const data = await fs.readFile(fileName);
+    return data.toString();
+}
+
+async function main() {
+    const fileName = __dirname + '/fileUnderStudy.js'
+    const contents = await getFileContents(fileName)
+    const ast = jsdocParse.AstBuilder.build(contents, fileName)
+    // console.dir(ast.program, { depth: 100 })
+    console.dir(ast, { depth: 100 })
+}
+
+main()
